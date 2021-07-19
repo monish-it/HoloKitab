51d69a6a7bffe1a36d1924cb0c39c1ad28515c12
monish-it <monish200110@gmail.com>
2021-07-19 14:23:55 +0530

    no message

--- js/script.js
+++ js/script.js
@@ -128,25 +128,8 @@ jQuery(document).ready(function ($) {
 // ----------------------------------------------------------------------------------------------
 // Buy-card-effect
 // ----------------------------------------------------------------------------------------------
-var card = $(".card");
-
-$(document).on("mousemove", function (e) {
-  var ax = -($(window).innerWidth() / 2 - e.pageX) / 20;
-  var ay = ($(window).innerHeight() / 2 - e.pageY) / 10;
-  card.attr(
-    "style",
-    "transform: rotateY(" +
-      ax +
-      "deg) rotateX(" +
-      ay +
-      "deg);-webkit-transform: rotateY(" +
-      ax +
-      "deg) rotateX(" +
-      ay +
-      "deg);-moz-transform: rotateY(" +
-      ax +
-      "deg) rotateX(" +
-      ay +
-      "deg)"
-  );
-});
+VanillaTilt.init(document.querySelector(".buy-card"),{
+  max: 20,
+  speed: 400,
+  // glare: true,
+})
