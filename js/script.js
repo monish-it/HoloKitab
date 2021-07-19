eddb88e12031f31d181b6d3e9cbb3ce8eed95384
monish-it <monish200110@gmail.com>
2021-07-19 11:48:36 +0530

    no message

--- js/script.js
+++ js/script.js
@@ -125,3 +125,28 @@ jQuery(document).ready(function ($) {
   $(window).bind("orientationchange", ScaleSlider);
   //responsive code end
 });
+// ----------------------------------------------------------------------------------------------
+// Buy-card-effect
+// ----------------------------------------------------------------------------------------------
+var card = $(".card");
+
+$(document).on("mousemove", function (e) {
+  var ax = -($(window).innerWidth() / 2 - e.pageX) / 20;
+  var ay = ($(window).innerHeight() / 2 - e.pageY) / 10;
+  card.attr(
+    "style",
+    "transform: rotateY(" +
+      ax +
+      "deg) rotateX(" +
+      ay +
+      "deg);-webkit-transform: rotateY(" +
+      ax +
+      "deg) rotateX(" +
+      ay +
+      "deg);-moz-transform: rotateY(" +
+      ax +
+      "deg) rotateX(" +
+      ay +
+      "deg)"
+  );
+});
